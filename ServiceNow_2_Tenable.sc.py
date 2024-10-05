da354240a02250dd4767e771a15aef10a8b15465
Zemolino <47012277+zemolino@users.noreply.github.com>
2024-10-05 13:32:48 +0200

    Update ServiceNow_2_Tenable.sc.py

--- ServiceNow_2_Tenable.sc.py
+++ ServiceNow_2_Tenable.sc.py
@@ -4,7 +4,7 @@ Name:           ServiceNow_2_Tenable.sc.py (Main Script)
 
 Date:           11/09/2019 
 
-Last update:    21/01/2024 
+Last update:     05/10/2024 
 
 Purpose:        A custom Tenable.sc Asset Data integration with ServiceNow.
 
@@ -45,8 +45,8 @@ import os
 import sys
 import getpass
 import time
-from socket import inet_aton
 import struct
+from socket import inet_aton
 from configparser import ConfigParser
 from datetime import datetime
 import requests
@@ -57,181 +57,166 @@ import pyServiceNowAPI
 # Disable urllib3 warnings
 requests.packages.urllib3.disable_warnings()
 
-# Define global logging variables
+# Initialize global logging and configuration variables
 scriptloc = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
 scriptname = os.path.splitext(os.path.basename(__file__))[0]
+configfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.conf')
 
 # Initialize logging
 log_instance = Logger(scriptloc, scriptname)
 logger = log_instance.setup()
-logger.info('Running on Python version {}'.format(sys.version))
-
-# Global configuration file path
-configfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.conf')
+logger.info(f'Running on Python version {sys.version}')
 
 # Global variables for Tenable and ServiceNow connections
 sc = None
 sn = None
 
-def initialize_tenable_sc():
+def initialize_tenable_sc(config):
+    """Initialize connection to Tenable.sc"""
     global sc
     try:
         sc_host = config.get('tenable.sc', 'SC_host')
         sc_username = config.get('tenable.sc', 'sc_username')
         sc_password = config.get('tenable.sc', 'sc_password')
 
-        # Create connection to tenable.sc server
         sc = pyTenableAPI.TenablescAPI(url=sc_host, username=sc_username, password=sc_password)
         sc.LoginTenable()
-        logger.info("Logged in successfully to tenable.sc!")
+        logger.info("Logged in successfully to Tenable.sc!")
     except Exception as e:
-        logger.error('Failed to connect to tenable.sc server')
-        logger.error('Likely cause is incorrect credentials', exc_info=True)
+        logger.error('Failed to connect to Tenable.sc server', exc_info=True)
         close_exit(1)
 
-def initialize_servicenow():
+def initialize_servicenow(config):
+    """Initialize connection to ServiceNow"""
     global sn
     try:
         srv_now_url = config.get('SrvNow', 'SrvNow_url')
         srv_now_username = config.get('SrvNow', 'SrvNow_username')
         srv_now_password = config.get('SrvNow', 'SrvNow_password')
 
-        # Create connection to ServiceNow server
         sn = pyServiceNowAPI.SrvNowAPI(url=srv_now_url, username=srv_now_username, password=srv_now_password)
         logger.info("Logged in successfully to ServiceNow!")
     except Exception as e:
-        logger.error('Failed to connect to ServiceNow server')
-        logger.error('Likely cause is incorrect credentials', exc_info=True)
+        logger.error('Failed to connect to ServiceNow server', exc_info=True)
         close_exit(1)
 
 def close_exit(exit_code):
-    """
-    Function to handle exiting the script either cleanly or with an error
-    exit_code - 0 for clean exit, 1 for exiting due to script error
-    """
+    """Handle script exit with proper cleanup"""
     if exit_code == 0:
-        logger.info('Script complete')
+        logger.info('Script completed successfully')
     else:
-        logger.info('Exiting script due to an error')
+        logger.error('Exiting script due to an error')
 
-    # Cleanly close the logging files
     log_instance.closeHandlers()
-    sys.exit()
+    sys.exit(exit_code)
 
 def update_tenable_assets(srv_name, ipaddr):
+    """Update or create Tenable.sc assets based on ServiceNow data"""
     try:
-        # Create assets if they are not already in tenable.sc
-        if srv_name not in [a['name'] for a in sc.HTTPRequest('GET', 'asset').json()['response']['usable']]:
-            # Create custom - static IP list - asset
+        existing_assets = sc.HTTPRequest('GET', 'asset').json()['response']['usable']
+        asset_names = [a['name'] for a in existing_assets]
+
+        if srv_name not in asset_names:
+            # Create new asset in Tenable.sc
             time.sleep(2)
             sc.HTTPRequest('POST', 'asset').json = {
                 'name': srv_name,
-                "description": "",
-                "groups": [],
+                'description': "",
+                'groups': [],
                 'definedIPs': ipaddr,
                 'type': 'static',
                 "tags": "SVC",
             }
-            logger.info(f'{srv_name} has been created')
+            logger.info(f'Created new asset: {srv_name}')
         else:
-            logger.info('\nThe asset already exists, updating IPs...')
-            for dict_item in sc.HTTPRequest('GET', 'asset').json()['response']['usable']:
-                if srv_name == dict_item['name']:
-                    # Update IP Addresses
+            # Update existing asset IP addresses
+            for asset in existing_assets:
+                if srv_name == asset['name']:
                     time.sleep(2)
-                    sc.HTTPRequest('PATCH', 'asset/{0}'.format(dict_item['id'])).json = {'definedIPs': ipaddr}
-                    logger.info('\nThe asset has been updated!')
+                    sc.HTTPRequest('PATCH', f'asset/{asset["id"]}').json = {'definedIPs': ipaddr}
+                    logger.info(f'Updated asset: {srv_name}')
     except Exception as e:
-        logger.error('Failed to create|update asset data into tenable.sc')
-        logger.error('Likely cause is the input parameters are malformed', exc_info=True)
+        logger.error('Failed to create or update asset in Tenable.sc', exc_info=True)
         close_exit(1)
 
 def delete_retired_assets(retired_asset):
+    """Delete retired assets from Tenable.sc"""
     try:
-        for asset in sc.HTTPRequest('GET', 'asset').json()['response']['usable']:
+        assets = sc.HTTPRequest('GET', 'asset').json()['response']['usable']
+        for asset in assets:
             if retired_asset == asset["name"]:
-                logger.info(f'{retired_asset} has been retired and deleted from Tenable.sc.')
-                sc.HTTPRequest('DELETE', 'asset/{0}'.format(asset['id']))
+                sc.HTTPRequest('DELETE', f'asset/{asset["id"]}')
+                logger.info(f'Deleted retired asset: {retired_asset}')
     except Exception as e:
-        logger.error('Failed to delete asset data from tenable.sc')
-        logger.error('Likely cause is the input parameters are malformed', exc_info=True)
+        logger.error('Failed to delete asset in Tenable.sc', exc_info=True)
         close_exit(1)
 
 def srv_now_asset_data():
-    srv_now_asset_lists = sn.HTTPRequest('GET', 'service_list').json()['result']['service_codes']
-    dict_srv_now = json.loads(srv_now_asset_lists)
-
-    for srv_now_list in dict_srv_now:
-        if srv_now_list["operational_status"] == "Retired":
-            delete_retired_assets(srv_now_list['code'])
-        else:
-            try:
-                logger.info("\n\n==============================================")
-                data = {'w_service_id': srv_now_list['code']}
-                srv_now_ip_list = sn.HTTPRequest('POST', 'asset', data=data).json()['result']
-                
-                if srv_now_ip_list['server_ip_list'] != "[]":
-                    ips_lists = json.loads(srv_now_ip_list['server_ip_list'])
-                    ips_prod = []
-
-                    for ip in ips_lists:
-                        if is_ipv4(ip['ip_address']) and ip['ip_address'] != "" and ip['ip_address'] != 'DHCP':
-                            if ip['used_for'] == "Production":
-                                if ip['ip_address'] not in ips_prod:
-                                    ips_prod.append(ip['ip_address'])
-                                else:
-                                    logger.info('Iterated IP %s' % ip['ip_address'])
-
-                    sorted_ips_prod = sorted(ips_prod, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
-                    srv_ips_prod = ', '.join(sorted_ips_prod)
-                    if srv_ips_prod != "":
-                        time.sleep(4)
-                        logger.info('\nThe service %s with production IPs: %s' % (srv_now_list['code'], srv_ips_prod))
-                        update_tenable_assets(srv_now_list['code'], srv_ips_prod)
-                        
-            except Exception as e:
-                logger.error('Failed to get data from ServiceNow function')
-                logger.error('Likely cause is the query is malformed', exc_info=True)
-                close_exit(1)
-
-def is_ipv4(i):
-    if '.' in i:
-        nodot_ip = i.split('.')
-    else:
-        return False
-    if len(nodot_ip) != 4:
-        return False
+    """Fetch and process asset data from ServiceNow"""
     try:
-        return all(0 <= int(p) < 256 for p in nodot_ip)
+        srv_now_assets = sn.HTTPRequest('GET', 'service_list').json()['result']['service_codes']
+        assets = json.loads(srv_now_assets)
+
+        for asset in assets:
+            if asset["operational_status"] == "Retired":
+                delete_retired_assets(asset['code'])
+            else:
+                process_active_assets(asset)
+    except Exception as e:
+        logger.error('Failed to fetch data from ServiceNow', exc_info=True)
+        close_exit(1)
+
+def process_active_assets(asset):
+    """Process active assets from ServiceNow"""
+    try:
+        data = {'w_service_id': asset['code']}
+        srv_now_ip_list = sn.HTTPRequest('POST', 'asset', data=data).json()['result']
+        ips_lists = json.loads(srv_now_ip_list['server_ip_list'])
+
+        if ips_lists:
+            ips_prod = [ip['ip_address'] for ip in ips_lists if is_ipv4(ip['ip_address']) and ip['used_for'] == "Production"]
+            if ips_prod:
+                sorted_ips_prod = sorted(ips_prod, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+                srv_ips_prod = ', '.join(sorted_ips_prod)
+                logger.info(f'Service {asset["code"]} with production IPs: {srv_ips_prod}')
+                update_tenable_assets(asset['code'], srv_ips_prod)
+    except Exception as e:
+        logger.error('Error processing active assets', exc_info=True)
+
+def is_ipv4(ip):
+    """Validate if a string is a valid IPv4 address"""
+    try:
+        parts = ip.split('.')
+        return len(parts) == 4 and all(0 <= int(part) < 256 for part in parts)
     except:
         return False
 
-if __name__ == '__main__':
+def setup_config():
+    """Setup or read configuration file"""
+    config = ConfigParser(delimiters=('=', ','))
     if not os.path.exists(configfile):
-        config = ConfigParser(delimiters=('=', ','))
         config.add_section('tenable.sc')
-        config.set('tenable.sc', 'SC_host', input('tenable.sc IP Address | Hostname : '))
-        config.set('tenable.sc', 'sc_username', input('tenable.sc Username : '))
-        config.set('tenable.sc', 'sc_password', getpass.getpass('tenable.sc Password : '))
+        config.set('tenable.sc', 'SC_host', input('Tenable.sc Hostname or IP: '))
+        config.set('tenable.sc', 'sc_username', input('Tenable.sc Username: '))
+        config.set('tenable.sc', 'sc_password', getpass.getpass('Tenable.sc Password: '))
 
         config.add_section('SrvNow')
-        config.set('SrvNow', 'SrvNow_url', input('SrvNow Service API link : '))
-        config.set('SrvNow', 'SrvNow_username', input('SrvNow Username : '))
-        config.set('SrvNow', 'SrvNow_password', getpass.getpass('SrvNow Password : '))
-
-        print('Stared Processing...')
+        config.set('SrvNow', 'SrvNow_url', input('ServiceNow API URL: '))
+        config.set('SrvNow', 'SrvNow_username', input('ServiceNow Username: '))
+        config.set('SrvNow', 'SrvNow_password', getpass.getpass('ServiceNow Password: '))
 
         with open(configfile, 'w') as f:
             config.write(f)
     else:
-        config = ConfigParser(delimiters=('=', ','))
         config.read(configfile)
+    return config
 
-        initialize_tenable_sc()
-        initialize_servicenow()
-
-        logger.info("Proccessing Methods...")
-        srv_now_asset_data()
+if __name__ == '__main__':
+    config = setup_config()
+    initialize_tenable_sc(config)
+    initialize_servicenow(config)
+    
+    logger.info("Processing ServiceNow asset data...")
+    srv_now_asset_data()
 
     close_exit(0)
-
